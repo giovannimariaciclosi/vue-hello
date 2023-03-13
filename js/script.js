ac89ad1470f9a26447944e926986505d0418dc78
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-13 12:18:23 +0100

    feat: inizializzazione esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+CONSEGNA: 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+import { createApp } from 'vue'
+
+createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+}).mount('#app')
